a1a684326b26c6480fd18f4c69a28f41ec926af3
noble-shake <coppgabliw@gmail.com>
2026-03-25 00:25:23 +0900

    🔨[ADD]:  R3, VContainer, UniTask, NetonSoft Extentions 추가

---
+++ Assets/Dev/Bootstrapper/MainLifetimeScope.cs
@@ -0,0 +1,15 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Extention.Vcontainer
+{
+    public class MainLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+
+        }
+    }
+
+}
+
