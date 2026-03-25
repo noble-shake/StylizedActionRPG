eef9eb7a986b1043b8166a144bc7e2acb2cd5cb8
noble-shake <coppgabliw@gmail.com>
2026-03-26 00:45:27 +0900

    Addressable Setup

--- Assets/Dev/Cores/CoreInterfaces.cs
+++ Assets/Dev/Cores/CoreInterfaces.cs
@@ -1,9 +1,22 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
 namespace RottenNoble.Cores
 {
     public interface IResourceManager
     {
-        void Init();
+        UniTask<T> LoadAsync<T>(string path, Action<string, float> onProgress = null) where T : UnityEngine.Object;
+        UniTask<List<T>> LoadsAsync<T>(List<string> paths, Action<string, float> onProgress = null) where T : UnityEngine.Object;
 
+        UniTask<T> CreateInstanceAsync<T>(string path, Transform parent) where T : UnityEngine.Object;
+        UniTask<List<T>> CreateInstancesAsync<T>(List<string> paths, Transform parent) where T : UnityEngine.Object;
+
+        UniTask<List<T>> LoadByLabelAsync<T>(string label, Action<string, float> onProgress = null) where T : UnityEngine.Object;
+
+        void DeleteInstance(UnityEngine.Object instanceObject);
+        void Dispose();
     }
 
     public interface IResourceFactory
